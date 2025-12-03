d645551ab59efb1e83c08c56e8dd539dd1d61784
Mehdi <mehdi.el.mestari.pro@gmail.com>
2025-12-03 15:28:43 +0100

    chore(frontend): setup dev and prod environments

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  // URL du json-server local
+  apiUrl: 'http://localhost:3000'
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  // URL du futur backend de Nelson (on met un placeholder pour l'instant)
+  apiUrl: 'https://api.moodify.com'
+};
