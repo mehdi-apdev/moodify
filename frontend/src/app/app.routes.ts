05746105cd250abdc0f433ec7b981e59224dccd2
Mehdi <mehdi.el.mestari.pro@gmail.com>
2025-12-09 16:02:46 +0100

    feat(playlist): implement playlist view, services and mock data

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PlaylistViewComponent } from './features/playlist-view/playlist-view';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'playlist', pathMatch: 'full' },
+  { path: 'playlist', component: PlaylistViewComponent }
+];
--- frontend/src/app/core/services/spotify.ts
+++ frontend/src/app/core/services/spotify.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '@env/environment';
+import { TrackDTO } from '@shared/api-contracts';
 
 @Injectable({
   providedIn: 'root',
 })
-export class Spotify {
-  
+export class SpotifyService {
+  // Injection de dépendance
+  private http = inject(HttpClient);
+
+  // Récupération de l'URL depuis l'environnement (Mock ou Prod)
+  private apiUrl = environment.apiUrl;
+
+  /**
+   * Récupère les recommandations
+   * Retourne un Observable qui contient une liste de TrackDTO
+   */
+  getRecommendations(): Observable<TrackDTO[]> {
+    // Appel HTTP GET vers http://localhost:3000/recommendations
+    return this.http.get<TrackDTO[]>(`${this.apiUrl}/recommendations`);
+  }
 }
